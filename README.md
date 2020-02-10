9736e5fd58494faac3788d2198a4117d306dd09b
Kartikaya Gupta <kats@trevize.staktrace.com>
2020-02-09 21:05:57 -0500

    Documentation and README

---
+++ README.md
@@ -0,0 +1,22 @@
+mnumonic
+===
+
+This is a tiny Rust library that allows you to convert binary byte data to and from a phrase that is memorable for a human.
+It is useful for cases where you need a human to transfer certain information (e.g. an opaque identifier) between systems where copy/paste may not be readily available.
+Instead of asking them to type in an opaque number like 3735928559, you can instead have them type in "sweet pump second tree".
+
+Currently only English is supported, but the code is written to allow other languages too. PRs to add word files for other languages are welcome.
+
+See API documentation at [docs.rs](https://docs.rs/staktrace/mnumonic/) for full details.
+
+Example usage
+---
+
+```rust
+use mnumonic;
+let opaque_identifier : u32 = 0xDEADBEEF;
+let human_readable = mnumonic::encode_u32_joined(opaque_identifier);
+println!("Instead of remembering '{}' (difficult), you can remember '{}' (easier)", opaque_identifier, human_readable);
+let back_to_identifier = mnumonic::decode_u32_joined(&human_readable).unwrap();
+assert_eq!(opaque_identifier, back_to_identifier);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+/// Return the list of words used for doing conversion. The words are
+/// read from a source file at compile time. The word list must have
+/// exactly 256 words, all in lowercase, and sorted alphabetically so
+/// that the list can be binary-searched. Care should be taken to avoid
+/// words that are commonly mis-spelled or that may have multiple valid
+/// spellings.
 fn words() -> Vec<&'static str> {
     #[cfg(feature = "en")]
     let list = include_str!("../words/en.txt");
@@ -8,6 +14,8 @@ fn words() -> Vec<&'static str> {
     words
 }
 
+/// Encode an array of bytes into a corresponding vector of human readable words.
+/// The output vector will be the same length as the input vector.
 pub fn encode_bytes(bytes: &[u8]) -> Vec<&'static str> {
     let words = words();
     let mut result = Vec::new();
@@ -17,6 +25,9 @@ pub fn encode_bytes(bytes: &[u8]) -> Vec<&'static str> {
     result
 }
 
+/// Encode a u32 into a vector of words. The output vector will have at least one
+/// entry, and no more than four entries; each entry is a single human-language word.
+/// Each byte of the u32 is converted to a word, with leading zero bytes dropped.
 pub fn encode_u32(num: u32) -> Vec<&'static str> {
     let mut bytes = vec![
         ((num >> 24) & 0xFF) as u8,
@@ -35,6 +46,19 @@ pub fn encode_u32(num: u32) -> Vec<&'static str> {
     encode_bytes(&bytes)
 }
 
+/// Same as encode_u32, except this also joins the individual words into a
+/// phrase. The words are joined using an ascii space character as delimiter.
+pub fn encode_u32_joined(num: u32) -> String {
+    encode_u32(num).join(" ")
+}
+
+/// Decode a vector of words (as produced by `encode_bytes`) back into the
+/// vector of bytes that produced them. In case of error (e.g. the provided
+/// vector has an invalid word), the index of the invalid entry is returned
+/// as an `Err`. Note that the words are lowercased per Unicode rules before
+/// decoding. This allows for scenarios where the human transcribing the
+/// words mixes up the case of the words, or some other system's autocorrect
+/// mutates the case.
 pub fn decode_bytes(encoded_words: &[&str]) -> Result<Vec<u8>, usize> {
     let mut result = Vec::new();
     let words = words();
@@ -45,6 +69,10 @@ pub fn decode_bytes(encoded_words: &[&str]) -> Result<Vec<u8>, usize> {
     Ok(result)
 }
 
+/// Decode a vector of words (as produced by `encode_u32`) back into the
+/// u32 that produced them. If the provided word vector has more than 4
+/// words, then an `Err(4)` is produced; if any the words are invalid, then
+/// an `Err` is produced with the index of the bad entry in the input vector.
 pub fn decode_u32(encoded_words: &[&str]) -> Result<u32, usize> {
     let bytes = decode_bytes(encoded_words)?;
     if bytes.len() > 4 {
@@ -55,6 +83,13 @@ pub fn decode_u32(encoded_words: &[&str]) -> Result<u32, usize> {
         result = (result << 8) | (b as u32 & 0xFF);
     }
     Ok(result)
+}
+
+/// Decode a phrase (as produced by `encode_u32_joined`). Basically the
+/// same as `decode_u32` except it splits the phrase on whitespace to generate
+/// the words to decode.
+pub fn decode_u32_joined(joined: &str) -> Result<u32, usize> {
+    decode_u32(&joined.split_whitespace().collect::<Vec<&str>>())
 }
 
 #[cfg(test)]
@@ -75,6 +110,7 @@ mod tests {
         assert_eq!(decode_bytes(&[]).unwrap(), vec![]);
         assert_eq!(decode_bytes(&["swEET", "pump", "second", "TREE"]).unwrap(), vec![0xDE, 0xAD, 0xBE, 0xEF]);
         assert_eq!(decode_u32(&["ant", "star"]).unwrap(), 1234);
+        assert_eq!(decode_u32_joined("sweet pump second tree").unwrap(), 0xDEADBEEF);
     }
 
     #[test]
